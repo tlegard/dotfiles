1b7b3f0d78b7c731c5ebf9176dffb245dd2ddafc
Alex Ford <alex.ford@codetunnel.com>
2014-11-14 10:26:03 -0700

    Moved .bash_profile contents to .bashrc and sourced .bashrc from .bash_profile. This should make things consistent across platforms.

--- install.sh
+++ install.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
+
+# Remove all dotfiles from the home directory if present.
+echo Removing any existing dotfiles from your home directory.
+rm -rf ~/.vim ~/.vimrc ~/.bashrc ~/.bash_profile ~/.inputrc ~/.gitconfig ~/.shell_prompt.sh ~/.tmux.conf ~/.tmux_theme
+
+# Initialize symlinks.
+echo Creating symlinks in your home directory that point to this dotfiles repository.
 ln -s "$PWD/.vim" ~/.vim
 ln -s "$PWD/.vim/vimrc" ~/.vimrc
+ln -s "$PWD/.bashrc" ~/.bashrc
 ln -s "$PWD/.bash_profile" ~/.bash_profile
 ln -s "$PWD/.inputrc" ~/.inputrc
 ln -s "$PWD/.gitconfig" ~/.gitconfig
 ln -s "$PWD/.shell_prompt.sh" ~/.shell_prompt.sh
 ln -s "$PWD/.tmux.conf" ~/.tmux.conf
 ln -s "$PWD/.tmux_theme" ~/.tmux_theme
-echo Finished.
+
+# Finished.
+echo Dotfiles installation complete.
