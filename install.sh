bab20478393b400351952fefbf16ab3c9f7a6699
Alex Ford <alex.ford@codetunnel.com>
2014-12-15 10:00:13 -0700

    Added tmux plugins.

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # Remove all dotfiles from the home directory if present.
 echo Removing any existing dotfiles from your home directory.
-rm -rf ~/.vim ~/.vimrc ~/.bashrc ~/.bash_profile ~/.inputrc ~/.gitconfig ~/.shell_prompt.sh ~/.tmux.conf ~/.tmux_theme
+rm -rf ~/.vim ~/.vimrc ~/.bashrc ~/.bash_profile ~/.inputrc ~/.gitconfig ~/.shell_prompt.sh ~/.tmux.conf ~/.tmux_theme ~/.tmux
 
 # Initialize symlinks.
 echo Creating symlinks in your home directory that point to this dotfiles repository.
@@ -15,6 +15,7 @@ ln -s "$PWD/.gitconfig" ~/.gitconfig
 ln -s "$PWD/.shell_prompt.sh" ~/.shell_prompt.sh
 ln -s "$PWD/.tmux.conf" ~/.tmux.conf
 ln -s "$PWD/.tmux_theme" ~/.tmux_theme
+ln -s "$PWD/.tmux" ~/.tmux
 
 # Finished.
 echo Dotfiles installation complete.
